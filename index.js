1f469e567d9e772e34c7501b48c8275f7c99e7bd
stoneymike <michaelstone730@gmail.com>
2022-11-08 09:03:59 -0500

    mail config 4

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.post("/import", async (req, res) => {
         res.redirect("/error")
         let transport = nodemailer.createTransport({
             host: process.env.HOST,
+            secure: true,
             auth: {
                 user: process.env.USER,
                 pass: process.env.PASS
